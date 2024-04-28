837ebec7dd693dc0311ad275cfbb1bfd259bcf4c
ayonhidayat <skylingling7@gmail.com>
2024-04-28 11:52:33 +0700

    membuat web geoper netuka

---
+++ js/index.js
@@ -0,0 +1,6 @@
+const eksplorButton = document.querySelector(".eksplor-button");
+const landingPage = document.querySelector(".landing-page");
+
+function removePage() {
+  landingPage.classList.add("hidden");
+}
