ace35315e4f726b8168dc72c9c5ab9e76f2e2e75
ayonhidayat <skylingling7@gmail.com>
2024-05-08 18:09:29 +0700

    update 0.3

--- js/index.js
+++ js/index.js
@@ -1,6 +1,12 @@
 const eksplorButton = document.querySelector(".eksplor-button");
 const landingPage = document.querySelector(".landing-page");
+const menu = document.querySelector("#menu");
+const navbarNav = document.querySelector(".navbar-nav");
 
 function removePage() {
   landingPage.classList.add("hidden");
+}
+
+function activeNavbar() {
+  navbarNav.classList.toggle("active");
 }
